089957b53b0d851a2b419bdca14f4a153c8f00d4
Miguel do Nascimento Evangelista <miguel.evangelista4@etec.sp.gov.br>
2025-10-20 21:54:35 +0000

    ex7

---
+++ ex7.js
@@ -0,0 +1,22 @@
+let input = require('readline-sync');
+let n1 = input.questionInt("Digite qualquer numero: ");
+let n2 = input.questionInt("Digite qualquer numero: ");
+
+let maior = n1
+let menor = n2 
+    
+while (n1 !== 0, n2 !== 0) {
+    n1 = input.questionInt("Digite qualquer numero: ");
+    n2 = input.questionInt("Digite qualquer numero: ");
+    if (n1>maior) {
+        maior = n1
+    }
+
+    if (n2<menor) {
+        menor = n2  
+    } 
+        
+}
+
+console.log(maior);
+console.log(menor);
